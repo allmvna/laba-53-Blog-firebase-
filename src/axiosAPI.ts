f5c9ccde491bfc5397ea5a78245cca1b68ff6dc7
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-25 03:22:42 +0600

    Added axios configuration for Firebase

---
+++ src/axiosAPI.ts
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+ const axiosAPI = axios.create({
+    baseURL: 'https://begimai-firebase-default-rtdb.europe-west1.firebasedatabase.app/',
+});
+
+ export default axiosAPI;
